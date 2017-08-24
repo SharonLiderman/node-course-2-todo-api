508bdcc478b5eafd5cd6a6f6e8702af4c6a98980
sharonmarmor <sharonmarmor@gmail.com>
2017-08-24 11:58:58 -0700

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res)=>{
 });
 
 app.listen(port, ()=>{
-    console.log(`Started on port ${port}`);
+    console.log(`Starting server on port ${port}`);
 });
 
 module.exports = {app};
